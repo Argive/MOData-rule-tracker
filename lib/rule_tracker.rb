1170859a46ba1aeadb40a9ae684cc280ea061cdc
yihwan <yihwankim@gmail.com>
2018-11-24 19:27:50 -0800

    refactor rule_tracker

--- lib/rule_tracker.rb
+++ lib/rule_tracker.rb
@@ -2,25 +2,26 @@ require 'PDF-Reader'
 require_relative 'rule_tracker/final_rules_parser'
 require_relative 'rule_tracker/proposed_rules_parser'
 
-# year = "2017"
-#
-# Dir.entries("../data/mo_register/#{year}").each do |file|
-#   if file.include?('txt')
-#
-#     if file.include?('all')
-#       final_rules_parser("../data/mo_register/#{year}/#{file}")
-#       proposed_rules_parser("../data/mo_register/#{year}/#{file}")
-#     elsif file.include?('proposed')
-#       proposed_rules_parser("../data/mo_register/#{year}/#{file}")
-#     elsif file.include?('final')
-#       final_rules_parser("../data/mo_register/#{year}/#{file}")
-#     else
-#       next
-#     end
-#
-#   end
-# end
+year = "2018"
+files = Dir.entries("../data/mo_register/#{year}").select { |file| file.include?('txt') }
+
+# enter the date at which the rule tracker should start, formatted as YYYY-MM-DD
+start_date = Date.parse('2018-07-12')
+
+files.each do |file|
+
+  next if Date.parse(file.split("_").first) < start_date
+
+  if file.include?('all') || file.include?('proposed')
+    proposed_rules_parser("../data/mo_register/#{year}/#{file}")
+  end
+
+  if file.include?('all') || file.include?('final')
+    final_rules_parser("../data/mo_register/#{year}/#{file}")
+  end
+
+end
 
 # for manual testing:
-proposed_rules_parser('../data/mo_register/2018/2018-07-02_proposed.txt')
-final_rules_parser('../data/mo_register/2018/2018-07-02_final.txt')
+# proposed_rules_parser('../data/mo_register/2018/2018-07-02_proposed.txt')
+# final_rules_parser('../data/mo_register/2018/2018-07-02_final.txt')
